642b539c2f235df01745fcb8784caf36d937ccc2
Oliver Morris <officialgamermorris@gmail.com>
2017-11-15 18:57:33 +0000

    Add files via upload

---
+++ maxcoin.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# encoding=utf8
+import sys
+import time
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+import json
+import urllib2
+
+def GetBlockCount():
+    query = urllib2.urlopen('http://api.maxcoinhub.io/Blockchain/GetBlockCount')
+
+    query = query.read()
+
+    j = json.loads(query)
+
+    s = j['result']
+
+    new = str(s)
+
+    print 'Block Count: ' + new
+
+    return GetBlockCount()
+
+def GetBlockHash():
+    query = urllib2.urlopen('http://api.maxcoinhub.io/Blockchain/GetBlockCount')
+
+    query = query.read()
+
+    j = json.loads(query)
+
+    s = j['result']
+
+    new = str(s)
+
+    url = urllib2.urlopen('http://api.maxcoinhub.io/Blockchain/GetBlockHash/' + new)
+
+    url = url.read()
+
+    j = json.loads(url)
+
+    results = j['result']
+
+    code = str(results)
+
+    print 'Block Hash ' + code
+
+    return GetBlockHash()
+
+
+def GetBlock():
+    query = urllib2.urlopen('http://api.maxcoinhub.io/Blockchain/GetBlockCount')
+
+    query = query.read()
+
+    j = json.loads(query)
+
+    s = j['result']
+
+    new = str(s)
+
+    url = urllib2.urlopen('http://api.maxcoinhub.io/Blockchain/GetBlockHash/' + new)
+
+    url = url.read()
+
+    j = json.loads(url)
+
+    results = j['result']
+
+    code = str(results)
+
+    new = urllib2.urlopen('http://api.maxcoinhub.io/Blockchain/GetBlock/' + code)
+
+    new = new.read()
+
+    j1  = json.loads(new)
+
+    return GetBlock()
+
+
---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
+
+setup(
+  name='python-MaxCoin-Wrapper',
+  version='1.0.0',
+  description='MaxCoin API wrapper for Python 2.7',
+  long_description=long_description,
+  url='https://github.com/olliecad1/python-MaxCoin-Wrapper',
+  author='Oliver Morris',
+  author_email='officialgamermorris@gmail.com',
+  license='MIT',
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+  ],
+  keywords='cryptocurrency Maxcoin Blockchain sha3',
+#  packages=find_packages(exclude=['contrib', 'docs', 'tests', 'test', 'dist']),
+  # install_requires=['peppercorn'],
+  # extras_require={
+  #   'dev': ['check-manifest'],
+  #   'test': ['coverage'],
+  # },
+  # package_data={
+  #   'sample': ['package_data.dat'],
+  # },
+  # data_files=[('my_data', ['data/data_file'])],
+  # entry_points={
+  #   'console_scripts': [
+  #     'sample=sample:main',
+  #   ],
+  # },
+)
